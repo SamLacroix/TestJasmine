3687115c401836290b858ca656a94af4628ba8bd
SamLacroix <33868670+SamLacroix@users.noreply.github.com>
2017-12-08 10:30:20 +0100

    Update calc.js

--- src/calc.js
+++ src/calc.js
@@ -1,36 +1,24 @@
 function calculatrice(signe, gauche, droite) {
-  var result;
     if (typeof gauche === 'number' && typeof droite === 'number') {
-	   	if (gauche>=0 && droite<430 && droite!=0) {
-			switch (signe) {
-				case "*":
-					result = gauche*droite;
-					return result;
-					break;
-				case "+":
-					result = gauche+droite;
-					return result;
-					break;
-				case "-":
-					result = gauche-droite;
-					return result;
-					break;
-				case "/":
-					result = gauche/droite;
-					return result;
-					break;
-				default:
-					result=null;
-					return result;
-			} 
-		} else {
-		   	result = null;
-		   	return result;
-		}
-	}else{
-		result=null;
-		return result;
-	}
+           if (gauche>=0 && droite<430 && droite!=0) {
+            switch (signe) {
+                case "*":
+                    return gauche*droite;
+                case "+":
+                    return gauche+droite;
+                case "-":
+                    return gauche-droite;
+                case "/":
+                    return gauche/droite;
+                default:
+                    return null;
+            } 
+        } else {
+               return null;
+        }
+    } else{
+        return null;
+    }
 }
  
 module.exports = calculatrice;
